8e5b324979ade20e63f4cc3789fd78071bb53727
Chris Cotton <chris.cottonn@gmail.com>
2015-03-25 16:54:48 -0700

    #my_each_with_index method added

--- enumerable.rb
+++ enumerable.rb
@@ -5,9 +5,15 @@ module Enumerable
         end
         self
     end
-end
 
+    def my_each_with_index
+        for i in 0...self.length
+            yield self[i], i    
+        end
+        self
+    end
+end
 test = ["a", "b", "c"]
-test.my_each do |letter|
-    print letter + " -- "
+test.my_each_with_index do |letter, index|
+    print letter + " -#{index}- "
 end
